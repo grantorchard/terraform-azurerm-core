5acf2cfb741334d2e1134248c0ed1b2ad9a63894
Grant Orchard <go@hashicorp.com>
2023-03-16 14:40:32 +1100

    add outputs

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,19 @@
+output "resource_groups" {
+  value = {
+    "sydney" = azurerm_resource_group.sydney.name,
+    "melbourne" = azurerm_resource_group.melbourne.name,
+    "canberra" = azurerm_resource_group.canberra.name,
+  }
+}
 
+output "workload_subnets" {
+  value = {
+    "sydney" = azurerm_subnet.sydney_workloads.id,
+    "melbourne" = azurerm_subnet.melbourne_workloads.id,
+    "canberra" = azurerm_subnet.canberra_workloads.id
+  }
+}
+
+output "firewall_name" {
+  value = azurerm_firewall.hub.name
+}
